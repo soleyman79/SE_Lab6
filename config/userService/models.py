082ae542638e7363c21dbc1f66960fde9d56cd95
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2023-08-24 06:43:29 +0330

    Create profile model

--- config/userService/models.py
+++ config/userService/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    credit = models.IntegerField(null=False, default=0)
+    totalLoan = models.SmallIntegerField(null=False, default=0)
+
+    def addCredit(self, amount):
+        self.credit += amount
+
+    def addLoan(self):
+        self.totalLoan += 1
+
+    def __str__(self) -> str:
+        return f'{self.user.username}'
