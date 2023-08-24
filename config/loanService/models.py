f9478730f31fdb21e74677edfad03976a6b8b524
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2023-08-24 07:32:23 +0330

    Create loan model

--- config/loanService/models.py
+++ config/loanService/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+from config.treasuryService.models import Book
+from config.userService.models import Profile
+
+
+class Loan(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrower = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    borrowDate = models.DateTimeField(auto_now_add=True)
+    returnDate = models.DateTimeField(auto_now=True)
