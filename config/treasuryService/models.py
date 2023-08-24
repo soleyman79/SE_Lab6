e68e9657da7c665f907c579d56346f2009d49742
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2023-08-24 06:52:09 +0330

    Create book model

--- config/treasuryService/models.py
+++ config/treasuryService/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=128, null=False)
+    pageNumbers = models.SmallIntegerField(null=True)
+    isAvailable = models.BooleanField(default=True)
+
+    def borrow(self):
+        if not self.isAvailable:
+            raise Exception('The book is unavailable.')
+        else:
+            self.isAvailable = False
+
+    def giveBack(self):
+        self.isAvailable = True
